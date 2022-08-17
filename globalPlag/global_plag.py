b950789a098d855bf0b3695218a44460257c2715
adityavk7 <95609661+adityavk7@users.noreply.github.com>
2022-08-17 18:55:46 +0530

    Update global_plag.py

--- globalPlag/global_plag.py
+++ globalPlag/global_plag.py
@@ -10,11 +10,8 @@ import search_google
 warnings.filterwarnings("ignore")
 warnings.filterwarnings("ignore", module='bs4')
 
-def search_bing(query, num):
-    return search_google.scrape_google(query, num)
-
 def extract_text(url):
-    page = requests.get(url)
+    page = requests.get(url, verify=False)
     soup = bs(page.text, 'html.parser')
     return soup.get_text()
 
@@ -26,24 +23,21 @@ def purify_text(string):
 def web_verify(string, results_per_sentence):
     sentences = nltk.sent_tokenize(string)
     matching_sites = []
-    for url in search_bing(query=string, num=results_per_sentence):
+    for url in search_google.scrape_google(string, results_per_sentence):
         matching_sites.append(url)
     for sentence in sentences:
-        for url in search_bing(query = sentence, num = results_per_sentence):
+        for url in search_google.scrape_google(sentence, results_per_sentence):
             matching_sites.append(url)
 
     return (list(set(matching_sites)))
 
-def similarity(str1, str2):
-    return compare.sim_score(str1, str2) * 100
-
 def report(text):
 
-    matching_sites = web_verify(purify_text(text), 2)
+    matching_sites = web_verify(purify_text(text), 20)
     matches = {}
 
     for i in range(len(matching_sites)):
-        matches[matching_sites[i]] = similarity(text, extract_text(matching_sites[i]))
+        matches[matching_sites[i]] = compare.sim_score(text, extract_text(matching_sites[i])) * 100
 
     matches = {k: v for k, v in sorted(matches.items(), key=lambda item: item[1], reverse=True)}
 
@@ -61,7 +55,7 @@ def get_global_score(essay):
 
 if __name__ == '__main__':
     tic = time.time()
-    res = report('''Google LLC (/ˈɡuːɡəl/ (listen)) is an American multinational technology company that focuses on search engine technology, online advertising, cloud computing, computer software, quantum computing, e-commerce, artificial intelligence,[9] and consumer electronics. It has been referred to as the "most powerful company in the world"[10] and one of the world's most valuable brands due to its market dominance, data collection, and technological advantages in the area of artificial intelligence.[11][12][13] It is considered one of the Big Five American information technology companies, alongside Amazon, Apple, Meta, and Microsoft.''')
+    res = report('''Google LLC is an American multinational technology company that focuses on search engine technology, online advertising, cloud computing, computer software, quantum computing, e-commerce, artificial intelligence, and consumer electronics. It has been referred to as the "most powerful company in the world" and one of the world's most valuable brands due to its market dominance, data collection, and technological advantages in the area of artificial intelligence. It is considered one of the Big Five American information technology companies, alongside Amazon, Apple, Meta, and Microsoft.''')
     i = 0
     for k,v in res.items():
         if i > 5: break
