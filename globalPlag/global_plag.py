c8034704f49ca37830d6e2dc747db9be18fb4936
adityavk7 <95609661+adityavk7@users.noreply.github.com>
2022-08-18 19:18:54 +0530

    Update global_plag.py

--- globalPlag/global_plag.py
+++ globalPlag/global_plag.py
@@ -11,9 +11,13 @@ warnings.filterwarnings("ignore")
 warnings.filterwarnings("ignore", module='bs4')
 
 def extract_text(url):
-    page = requests.get(url, verify=False)
-    soup = bs(page.text, 'html.parser')
-    return soup.get_text()
+    try:
+        page = requests.get(url, timeout=15)
+        soup = bs(page.text, 'html.parser')
+        res = soup.get_text()
+    except:
+        res = None
+    return res
 
 def purify_text(string):
     stop_words = set(nltk.corpus.stopwords.words('english'))
@@ -31,17 +35,35 @@ def web_verify(string, results_per_sentence):
 
     return (list(set(matching_sites)))
 
+# def report(text):
+
+    # matching_sites = web_verify(purify_text(text), 20)
+    # matches = {}
+
+    # for i in range(len(matching_sites)):
+        # matches[matching_sites[i]] = compare.sim_score(text, extract_text(matching_sites[i])) * 100
+
+    # matches = {k: v for k, v in sorted(matches.items(), key=lambda item: item[1], reverse=True)}
+
+    # return matches
+    
 def report(text):
-
-    matching_sites = web_verify(purify_text(text), 20)
-    matches = {}
-
-    for i in range(len(matching_sites)):
-        matches[matching_sites[i]] = compare.sim_score(text, extract_text(matching_sites[i])) * 100
-
-    matches = {k: v for k, v in sorted(matches.items(), key=lambda item: item[1], reverse=True)}
-
-    return matches
+    res = {}
+    txt_sent = nltk.sent_tokenize(text)
+    for i in range(0, len(txt_sent), 2):
+        sent = txt_sent[i] + ' ' + txt_sent[i+1]
+        sites = search_google.scrape_google(sent, 5)
+        matches = ('', 0)
+        for i in range(len(sites)):
+            extracted = extract_text(sites[i])
+            if extracted:
+                temp = compare.sim_score(sent, extracted) * 100
+                if temp > matches[1]:
+                    matches = (sites[i], temp)
+        # matches = {k: v for k, v in sorted(matches.items(), key=lambda item: item[1], reverse=True)}
+        res[sent] = matches #list(matches.items())[0]
+    return res
+    
 
 def get_global_score(essay):
     scores = ''
@@ -55,10 +77,18 @@ def get_global_score(essay):
 
 if __name__ == '__main__':
     tic = time.time()
-    res = report('''Google LLC is an American multinational technology company that focuses on search engine technology, online advertising, cloud computing, computer software, quantum computing, e-commerce, artificial intelligence, and consumer electronics. It has been referred to as the "most powerful company in the world" and one of the world's most valuable brands due to its market dominance, data collection, and technological advantages in the area of artificial intelligence. It is considered one of the Big Five American information technology companies, alongside Amazon, Apple, Meta, and Microsoft.''')
-    i = 0
-    for k,v in res.items():
-        if i > 5: break
-        print('\n', k, ': ', v)
-        i += 1
-    print('time: ', time.time() - tic)
+    print(time.ctime())
+    q = '''Blockchain in simple terms is regarded as the record-keeping technology and has been popular after the advent of Bitcoin. The information regarding different transactions and details of credit and debit are noted down by us and are termed as records. This is the procedure of maintaining the records manually and the manual records are stated as the ledger. The process of maintaining the record of information and data in form of databases that are stored electronically on the computer system is called Blockchain. It can also be regarded as a digital ledger.
+        Blockchain technology innovation is proliferating in the hedge fund industry. Blockchain technology plays a primary role in front office and investment functions, in the securing of crypto assets, but also in private investment fund managers’ attempts to satisfy the growth expectations of clients. Although the use of blockchain technology in private investment fund strategies is still in its infancy, as it evolves and accelerates, the associated innovation benefits promise lasting change for the industry.
+        Google LLC is an American multinational technology company that focuses on search engine technology, online advertising, cloud computing, computer software, quantum computing, e-commerce, artificial intelligence, and consumer electronics. It has been referred to as the "most powerful company in the world"[10] and one of the world's most valuable brands due to its market dominance, data collection, and technological advantages in the area of artificial intelligence.'''
+    res = report(q)
+    # i = 0
+    # for k,v in res.items():
+        # if i > 5: break
+        # print(k, ': ', v)
+        # i += 1
+    tic = time.time() - tic
+    for k, v in res.items():
+        print(k, v, '\n')
+    print('\ntime: ', tic)
+    
