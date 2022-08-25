6e6c3dd50163f14628e61040bd0f72ad2a25ea68
shishir-m <75567472+shishir-m@users.noreply.github.com>
2022-08-25 20:19:14 +0530

    Optimized code
    Fixed issue of text file not being closed.

--- grammar/spell_check.py
+++ grammar/spell_check.py
@@ -5,24 +5,19 @@ def check(name):
     tool = language_tool_python.LanguageTool('en-US')  
     with open('%s.txt' %name) as f:
         lines = f.readlines()
+    f.close()
     text = str(lines)
     # getting the matches  
     my_matches = tool.check(text)  
     
-    # defining some variables  
     myMistakes = [] 
-    startPositions = []  
-    endPositions = []  
     
-    # using the for-loop  
     for rules in my_matches:  
         if len(rules.replacements) > 0:  
-            startPositions.append(rules.offset)  
-            endPositions.append(rules.errorLength + rules.offset)  
             myMistakes.append(text[rules.offset : rules.errorLength + rules.offset])  
     count = len(myMistakes)
-    print(myMistakes)
-    print(count)
+    print("List of spelling errors:",myMistakes)
+    print("Count",count)
 if __name__ == "__main__":
-    name = "input"
+    name = "input" #tbi
     check(name)
