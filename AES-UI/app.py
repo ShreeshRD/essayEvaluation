61cd8a9ece45196211bfa29720a27ea9cc6b893c
ShreeshRD <93830912+ShreeshRD@users.noreply.github.com>
2022-10-08 15:21:11 +0530

    Added prompts, UI changes and Scoring

--- AES-UI/app.py
+++ AES-UI/app.py
@@ -1,20 +1,12 @@
 from flask import Flask, url_for, render_template, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
-import os
-from werkzeug.utils import secure_filename
-
-import OCR
+# from essaychecker import evaluate
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-
-UPLOAD_FOLDER = 'D:/capstone project/CODE/UI/AES-UI/uploads'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///aes.db'
 db = SQLAlchemy(app)
-
-#photos = UploadSet('photos', IMAGES)
-#configure_uploads(app, photos)
+bcrypt = Bcrypt(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,19 +17,41 @@ class User(db.Model):
         self.username = username
         self.password = password
 
+class Prompts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    min_marks = db.Column(db.Integer)
+    max_marks = db.Column(db.Integer)
+    source_essay = db.Column(db.String(100))
+
+# class Source(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     essay = db.Column(db.String(2000))
+
 @app.route('/', methods=['GET'])
 def index():
     if session.get('logged_in'):
-        return render_template('home.html')
+        prs = Prompts.query.all()
+        return render_template('home.html', prompts = prs)
     else:
         return render_template('index.html', message="Hello!")
 
+@app.route('/prompt/<int:sid>')
+def prompt(sid):
+    src = Prompts.query.get(sid)
+    html = None
+    if src.source_essay == "YES":
+        with open('p'+str(sid)+'.htm','r') as file:
+            html = file.read()
+    return render_template('prompt.html', no = str(sid), source = src, content = html)
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         try:
-            db.session.add(User(username=request.form['username'], password=request.form['password']))
+            hashed_password = bcrypt.generate_password_hash(request.form['password'])
+            db.session.add(User(username=request.form['username'], password=hashed_password))
             db.session.commit()
             return redirect(url_for('login'))
         except:
@@ -54,8 +68,8 @@ def login():
         u = request.form['username']
         p = request.form['password']
         session["uname"] = u
-        data = User.query.filter_by(username=u, password=p).first()
-        if data is not None:
+        data = User.query.filter_by(username=u).first()
+        if data is not None and bcrypt.check_password_hash(data.password, p):
             session['logged_in'] = True
             return redirect(url_for('index'))
         return render_template('index.html', message="Incorrect Details")
@@ -70,21 +84,13 @@ def logout():
 def takeInput():
     t = request.form['input_text']
     name = session["uname"]
-    d_file = open("%s.txt" %name,"w")
-    d_file.write(t)
-    d_file.close()
-    return redirect(url_for('index'))
-@app.route('/takeImg',methods=['POST'])
-def takeImg():
-    if request.files:
-        image = request.files["image"]
-        print(image)
-        image.save(os.path.join(app.config["UPLOAD_FOLDER"], image.filename))
-        text = OCR.getText(os.path.join(app.config["UPLOAD_FOLDER"], image.filename))
-        print(text)
-    return render_template("result.html", text=text)
+    # d_file = open("%s.txt" %name,"w")
+    # d_file.write(t)
+    # d_file.close()
+    score = "evaluate(t)"
+    return render_template('results.html', res = score)
 
 if(__name__ == '__main__'):
     app.secret_key = "ThisIsNotASecret:p"
     db.create_all()
-    app.run()
+    app.run(debug=True)
