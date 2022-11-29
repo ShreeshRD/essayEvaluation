85c05c475e6d86abb5fda07bf6e55e6b3b55c7d6
ShreeshRD <93830912+ShreeshRD@users.noreply.github.com>
2022-11-29 19:09:52 +0530

    Add files via upload

--- scripts/globalPlag/global_plag.py
+++ scripts/globalPlag/global_plag.py
@@ -21,8 +21,15 @@ def extract_text(url):
 def report(text):
     res = {}
     txt_sent = nltk.sent_tokenize(text)
-    for i in range(0, len(txt_sent), 2):
-        sent = txt_sent[i] + ' ' + txt_sent[i+1]
+    lines = 2 # change how many lines you want checked at a time
+    for i in range(0, len(txt_sent), lines):
+        j = 0
+        sent = ""
+        while (i + j) < len(txt_sent):
+            sent += txt_sent[i + j] + ' '
+            j += 1
+            if j >= lines:
+                break
         sites = search_google.scrape_google(sent, 5)
         matches = ('', 0)
         for i in range(len(sites)):
@@ -31,8 +38,7 @@ def report(text):
                 temp = compare.sim_score(sent, extracted) * 100
                 if temp > matches[1]:
                     matches = (sites[i], temp)
-        # matches = {k: v for k, v in sorted(matches.items(), key=lambda item: item[1], reverse=True)}
-        res[sent] = matches #list(matches.items())[0]
+        res[sent] = matches
     return res
     
 
@@ -41,21 +47,17 @@ def get_global_score(essay):
     res = report(essay)
     i = 0
     for k,v in res.items():
-        if i > 5: break
-        scores += ('\n' + str(k) + ': ' + str(v))
+        if v[1] > 30:
+            scores += (str(k) + '--> '+ str(round(v[1],1)) + '% match with: <a style="text-decoration: underline;" class="display-5" href=' + str(v[0])  + '>link<a/></br>')
         i += 1
     return scores
 
 if __name__ == '__main__':
     tic = time.time()
     print(time.ctime())
-    q = '''In a small saucepan, add finely chopped dark chocolate and a splash of cocoa powder. Stir occasionally until melted. While waiting for that to melt, mix together cornstarch with a small splash of whole milk.  Once chocolate is melted, add the rest of your milk slowly while constantly whisking. Continue to heat until all of it is nice and hot. Then whisk in the cornstarch slurry. Continue to heat until thickened. Pour into a mug, topped with whipped cream and dust with cocoa powder. '''
+    q = '''Dear local newspaper, I think effects computers have on people are great learning skills/affects because they give us time to chat with friends/new people, helps us learn about the globe(astronomy) and keeps us out of troble! Thing about! Dont you think so? How would you feel if your teenager is always on the phone with friends! Do you ever time to chat with your friends or buisness partner about things. Well now - there's a new way to chat the computer, theirs plenty of sites on the internet to do so: @ORGANIZATION1, @ORGANIZATION2, @CAPS1, facebook, myspace ect. Just think now while your setting up meeting with your boss on the computer, your teenager is having fun on the phone not rushing to get off cause you want to use it. How did you learn about other countrys/states outside of yours? Well I have by computer/internet, it's a new way to learn about what going on in our time! You might think your child spends a lot of time on the computer, but ask them so question about the economy, sea floor spreading or even about the @DATE1's you'll be surprise at how much he/she knows. Believe it or not the computer is much interesting then in class all day reading out of books. If your child is home on your computer or at a local library, it's better than being out with friends being fresh, or being perpressured to doing something they know isnt right. You might not know where your child is, @CAPS2 forbidde in a hospital bed because of a drive-by. Rather than your child on the computer learning, chatting or just playing games, safe and sound in your home or community place. Now I hope you have reached a point to understand and agree with me, because computers can have great effects on you or child because it gives us time to chat with friends/new people, helps us learn about the globe and believe or not keeps us out of troble. Thank you for listening.
+'''
     res = report(q)
-    # i = 0
-    # for k,v in res.items():
-        # if i > 5: break
-        # print(k, ': ', v)
-        # i += 1
     tic = time.time() - tic
     for k, v in res.items():
         print(k, v, '\n')
